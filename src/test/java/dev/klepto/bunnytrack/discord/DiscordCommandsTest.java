1bfe6259d4a225e664b5874297062d2870c0a9a1
kLeptO <klepto@inbox.lt>
2020-05-02 23:47:16 +0300

    Add classes for future unit tests

---
+++ src/test/java/dev/klepto/bunnytrack/discord/DiscordCommandsTest.java
@@ -0,0 +1,7 @@
+package dev.klepto.bunnytrack.discord;
+
+/**
+ * @author <a href="https://klepto.dev/">Augustinas R.</a>
+ */
+public class DiscordCommandsTest {
+}
---
+++ src/test/java/dev/klepto/bunnytrack/record/RecordsServiceTest.java
@@ -0,0 +1,7 @@
+package dev.klepto.bunnytrack.record;
+
+/**
+ * @author <a href="https://klepto.dev/">Augustinas R.</a>
+ */
+public class RecordsServiceTest {
+}
