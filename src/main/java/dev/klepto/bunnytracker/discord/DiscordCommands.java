da1c1174111b988fe104a3e8dcc354fa90e5fa8b
kLeptO <klepto@inbox.lt>
2020-05-03 00:27:09 +0300

    Add command for testing record embed

--- src/main/java/dev/klepto/bunnytracker/discord/DiscordCommands.java
+++ src/main/java/dev/klepto/bunnytracker/discord/DiscordCommands.java
@@ -1,9 +1,14 @@
 package dev.klepto.bunnytracker.discord;
 
+import dev.klepto.bunnytracker.record.Record;
+import dev.klepto.bunnytracker.record.api.ApiMapUtils;
 import dev.klepto.commands.annotation.Command;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import net.dv8tion.jda.api.entities.Message;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * @author <a href="https://klepto.dev/">Augustinas R.</a>
@@ -27,6 +32,17 @@ public class DiscordCommands {
     @Command
     public void ping(Message message) {
         message.getTextChannel().sendMessage("pong").queue();
+    }
+
+    @Command
+    public void testRecord(Message message) {
+        val players = new Record.Player[] {
+                new Record.Player("Chatouille", 1, 2, 123456, 125456, true),
+                new Record.Player("kleps", 2, 1, 123557, 123557, false),
+                new Record.Player("Ransom", 3, 3, 234567, 234567, false)
+        };
+        val record = new Record(ApiMapUtils.getMap("BT-RansomKek"), players[0], players);
+        bot.announceRecords(new HashSet<>(Arrays.asList(record)));
     }
 
 }
